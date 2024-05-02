c279f5b5d263731ba36d7706ca13621a43a17a47
RahulKathayat <rkathayat1103@gmail.com>
2024-05-03 03:16:12 +0530

    skeleton of ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
-import Link from "next/link";
 
 export default function HomePage() {
+
+  const mockData: string[] = [
+    "https://utfs.io/f/7bd88e21-dd54-4821-ad69-34dfc4f9815e-2sc149.png",
+    "https://utfs.io/f/b492653a-5520-43a4-82a0-fe8aa75d07f0-2sc0fa.png",
+    "https://utfs.io/f/7bd88e21-dd54-4821-ad69-34dfc4f9815e-2sc149.png",
+    "https://utfs.io/f/b492653a-5520-43a4-82a0-fe8aa75d07f0-2sc0fa.png",
+    "https://utfs.io/f/7bd88e21-dd54-4821-ad69-34dfc4f9815e-2sc149.png",
+    "https://utfs.io/f/b492653a-5520-43a4-82a0-fe8aa75d07f0-2sc0fa.png",
+  ];
+
+  const mockUrls = mockData.map((data, index) => (
+     {
+      id: index+1,
+      url: data,
+    }
+  ));
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        Hello there
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {
+          [...mockUrls,...mockUrls,...mockUrls].map((data)=>(
+            <div key={data.id} className="w-48 ">
+              <img src={data.url} alt="image" />
+            </div>
+          ))
+        }
+      </div>
     </main>
   );
 }
