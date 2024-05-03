e2511626b73d57d4f4ffb6149bf9ad433252aa3b
RahulKathayat <rkathayat1103@gmail.com>
2024-05-03 23:38:46 +0530

    added auth based routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,19 @@
-import { db } from "~/server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import Images from "./_components/Images";
 export const dynamic = "force-dynamic"
 
-export default async function HomePage() {
-  // fetch data in descending order by id
-  const images = await db.query.images.findMany({
-    orderBy: (model,{asc}) => asc(model.id),
-  });
+export default function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {
-          [...images,...images,...images].map((data)=>(
-            <div key={data.id} className="w-48 flex flex-col">
-              <img src={data.url} alt="image" />
-              <div>{data.name}</div>
-            </div>
-          ))
-        }
-      </div>
+      <SignedOut>
+        <div className="flex flex-col justify-center h-[36rem] overflow-hidden">
+          <h1 className="text-2xl font-bold text-center">Welcome to the Image Gallery</h1>
+          <h2 className="text-2xl font-bold text-center">Please Sign in to contiue ...</h2>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
