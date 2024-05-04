f0a523aa5191a5eb8c29c7b96ddb3d92ad6ae54c
RahulKathayat <rkathayat1103@gmail.com>
2024-05-04 19:59:20 +0530

    used next-image component for optimization

--- src/app/_components/Images.tsx
+++ src/app/_components/Images.tsx
@@ -1,13 +1,14 @@
+import Image from "next/image";
 import { getMyImages } from "~/server/db/queries";
 
 const Images = async () => {
     const images = await getMyImages();
     return (
-      <div className="flex flex-wrap gap-4">
+      <div className="flex flex-wrap gap-4 justify-center">
         {
           images.map((data)=>(
-            <div key={data.id} className="w-48 flex flex-col">
-              <img src={data.url} alt="image" />
+            <div key={data.id} className="w-48 h-48 flex flex-col">
+              <Image src={data.url} alt="image" style={{objectFit:"contain"}} width={192} height={192}/>
               <div>{data.name}</div>
             </div>
           ))
