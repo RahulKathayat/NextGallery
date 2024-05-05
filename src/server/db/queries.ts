499736cb9c67d78d9d701700a626cbb326f90c53
RahulKathayat <rkathayat1103@gmail.com>
2024-05-05 21:44:05 +0530

    fixed a bug

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,12 +1,12 @@
 import "server-only";
 import { auth } from "@clerk/nextjs/server";
 import { db } from "~/server/db";
-const user = auth();
-    
-if(!user.userId) throw new Error("Unauthorized");
 
 // fetch data in descending order by id
 export const getMyImages = async () => {
+    const user = auth();
+    if(!user.userId) throw new Error("Unauthorized");
+    
     const images = await db.query.images.findMany({
         orderBy: (model,{desc}) => desc(model.id),
         where: (model,{eq}) => eq(model.userId, user.userId),
@@ -15,7 +15,10 @@ export const getMyImages = async () => {
 };
 
 export const getImage = async (id: number) => {
+    const user = auth();
+    if(!user.userId) throw new Error("Unauthorized");
     if(!user) throw new Error("Unauthorized");
+    
     const image = await db.query.images.findFirst({
         where: (model,{eq}) => eq(model.id, id),
     });
